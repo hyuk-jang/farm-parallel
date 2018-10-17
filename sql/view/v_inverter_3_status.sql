0032e98cbcf5def265a3dc05bc60eb498592ff8c
장혁 <hyuk.jang@smsoft.co.kr>
2018-10-17 18:33:35 +0900

    @ 인버터 뷰 삼상 적용

---
+++ sql/view/v_inverter_3_status.sql
@@ -0,0 +1,20 @@
+SELECT 
+	rp.place_seq,
+	id.*,
+	ROUND((power_total_kwh - (SELECT MAX(power_total_kwh) FROM pw_inverter_data WHERE inverter_seq = id.inverter_seq AND writedate>= CURDATE() - 1 AND writedate< CURDATE())), 1) AS daily_power_kwh,
+	pv.amount AS pv_amount, pv.compose_count, pv.install_place AS install_place
+	FROM pw_inverter_data id
+	LEFT JOIN pw_inverter
+		ON pw_inverter.inverter_seq = id.inverter_seq
+	LEFT JOIN pw_relation_power rp
+		ON rp.inverter_seq = id.inverter_seq
+	LEFT JOIN pw_photovoltaic pv
+		ON pv.photovoltaic_seq = rp.photovoltaic_seq
+		
+		
+	WHERE inverter_data_seq IN (
+		SELECT MAX(inverter_data_seq)
+		FROM pw_inverter_data
+		GROUP BY inverter_seq
+	)
+	ORDER BY pw_inverter.chart_sort_rank
